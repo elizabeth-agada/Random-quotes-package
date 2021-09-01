6317113d215902745990c3da30ea5f9a9c504596
[Elizabeth Agada] <[elizadave6@gmail.com]>
2021-08-31 23:26:22 -0700

    test.php

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>random quotes</title>
+
+    </head>
+    <body>
+    <h2>Random quotes</h2>
 <?php 
 
 require_once './src/RandomQuotes.php';
@@ -7,3 +16,6 @@ $quote = new \RandomQuotes\RandomQuotes();
 print_r($quote->generate());
 
 echo "\n";
+?>
+</body>
+</html>
